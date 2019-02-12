cd03bd7b109e7d5ec406a807aa317bb7628fd338
Hunter <hbova@mail.pima.edu>
2019-02-12 16:34:41 -0700

    Created new hopefully working repo

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour {
+
+    public float rotationSpeed = 80;
+    public float moveSpeed = 5;
+
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player.transform.Rotate(new Vector3(0f, Time.deltaTime * rotationSpeed * Input.GetAxis("Horizontal"), 0f));
+        player.transform.Translate(new Vector3(0f,0f, Time.deltaTime * moveSpeed * Input.GetAxis("Vertical") * -1));
+    }
+}
